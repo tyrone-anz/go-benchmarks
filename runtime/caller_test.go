9f239a87ff07a25c9597759622d54d1416840f2c
Tyrone Villaluna <tyrone.villaluna@anz.com>
2023-04-04 19:55:41 +1000

    runtime.Caller with Frames.Next bench

--- runtime/caller_test.go
+++ runtime/caller_test.go
@@ -23,11 +23,30 @@ func BenchmarkRuntimeCallers(b *testing.B) {
 }
 
 func BenchmarkRuntimeCallersAndFrame(b *testing.B) {
+	const depthMax = 15
 	for n := 0; n < b.N; n++ {
-		pc := make([]uintptr, 15)
-		runtime.Callers(1, pc)
+		var pcs [depthMax]uintptr
+		n := runtime.Callers(1, pcs[:])
+		pc := pcs[:n]
+
 		runtime.CallersFrames(pc)
 	}
 
-	// BenchmarkRuntimeCallersAndFrame-10    	 3327930	       366.0 ns/op	     352 B/op	       2 allocs/op
+	// BenchmarkRuntimeCallersAndFrame-10    	 3292882	       364.3 ns/op	     352 B/op	       2 allocs/op
+}
+
+func BenchmarkRuntimeCallersAndFrameAndNextAll(b *testing.B) {
+	const depthMax = 15
+	for n := 0; n < b.N; n++ {
+		var pcs [depthMax]uintptr
+		n := runtime.Callers(1, pcs[:])
+		pc := pcs[:n]
+
+		frames := runtime.CallersFrames(pc)
+		for _, has := frames.Next(); has; {
+			_, has = frames.Next()
+		}
+	}
+
+	// BenchmarkRuntimeCallersAndFrameAndNextAll-10    	 1478937	       786.8 ns/op	     352 B/op	       2 allocs/op
 }
